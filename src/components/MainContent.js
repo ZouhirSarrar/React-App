deb87be4cc27b5fc3a11e5638f6a479376ac65fe
hackerrank <me@hackerrank.com>
2019-12-05 20:14:14 +0100

    adding first files

---
+++ src/components/MainContent.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+function MainContent(){
+    return(
+        <main>
+            <h1>Sarrar Zouhir</h1>
+            <p>i'm a big data student from Morocco</p>
+             <ul>
+                <li>Times square</li>
+                <li>Maldive</li>
+             </ul>
+        </main>
+    )
+}
+export default MainContent
---
+++ src/components/MyInfo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MyInfo () {
+    return(
+    <div>
+        <h1>Sarrar Zouhir</h1>
+        <p>i'm a big data student from Morocco</p>
+        <li>Times square</li>
+        <li>Maldive</li>
+    </div>
+    )
+}
+
+export default MyInfo
