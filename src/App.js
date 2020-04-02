ce7752a6d5ca392d15342bca3361aa31ef906644
hackerrank <me@hackerrank.com>
2020-04-02 15:34:57 +0100

    adding

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Joke from './components/Joke'
 function App(){
   return(
     <div>
-      <Joke joke ={{ question:"what is aspergers ?",punchline:"burgers in the ass"}}/>
-      <Joke joke ={{ question:"what is hey ?",punchline:"burgers "}}/>
-      <Joke joke ={{ question:"what is azaz ?",punchline:"aaaaa"}}/>
-      <Joke joke ={{ question:"what is a door ?",punchline:"door knob"}}/>
-      <Joke joke ={{ question:"what is aspergers ?",punchline:"fuck you cartman"}}/>
+      <Joke punchline="hey jude don't make it bad"/>
+
+      <Joke question="what is aspergers ?"punchline="burgers in the ass"/>
+      <Joke question="what is hey ?"punchline="burgers "/>
+      <Joke question="what is azaz ?"punchline="aaaaa"/>
+      <Joke question="what is a door ?"punchline="door knob"/>
+      <Joke question="what is aspergers ?"punchline="fuck you cartman"/>
 
     </div>
   )
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 function Joke(props){
     console.log(props)
+    if (props.question == null ){
+        return(
+            <div>
+            
+            <p>punchline: {props.punchline}</p>
+            </div>
+        )
+    }
+    else {
     return(
         <div>
-        <h2>question: {props.joke.question}</h2>
-        <p>punchline: {props.joke.punchline}</p>
+        <h2>question: {props.question}</h2>
+        <p>punchline: {props.punchline}</p>
         </div>
-    )
+    )}
 }
 
 export default Joke
